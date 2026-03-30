15b48f0f5cca93180db71b4b255a04abaebeee6a
student <student@itismeucci.com>
2026-03-30 10:40:39 +0200

    Fase quasi finale

--- build/init.sql
+++ build/init.sql
@@ -20,13 +20,13 @@ CREATE TABLE transactions (
 INSERT INTO 
   accounts (owner_name, currency)
 VALUES 
-  ('Mario Rossi', 'EUR'),
-  ('Luigi Bianchi', 'EUR');
+  ('Mario Rossi', '300000'),
+  ('Luigi Bianchi', '120000');
 
 INSERT INTO 
   transactions (account_id, type, amount, description)
 VALUES 
-  ( 1, 'deposit', 1000.00, 'stipendio'),
-  ( 2, 'deposit', 2000.00, 'stipendio'),
-  ( 1, 'deposit', 1200.00, 'scommesse');
+  ( 1, 'deposit', 100000 'stipendio'),
+  ( 2, 'deposit', 200000, 'stipendio'),
+  ( 1, 'deposit', 120000, 'scommesse');
 
--- php/index.php
+++ php/index.php
@@ -19,10 +19,84 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     $response->getBody()->write("Ciao, $name");
     return $response;
 });
+$app->post('/accounts/{id}/deposita', function (Request $request, Response $response) {
 
-// ottenere l'elenco dei movimenti
+    $conn = getConnection();
+    $data = $request->getParsedBody();
+    $account_id = $args['id'];
 
-$app->get('/accounts/{id}/transactions', "bankingControll:lista");
+    $stmt = $conn->prepare("INSERT INTO transactions (type, amount,description,account_id) VALUES (?, ?, ?,?)");
+    $stmt->bind_param("sisi","deposit", $data['amount'], $data['description'],$account_id);
+    $stmt->execute();
+
+    $response->getBody()->write(json_encode([
+        "message" => "Transazione eseguita",
+        "id" => $stmt->insert_id
+    ]));
+
+    $stmt = $conn->prepare("UPDATE accounts a SET currency = (select SUM(t.amount) from transactions t where t.account_id = ? ) WHERE a.id = ?");
+    $stmt->bind_param("ii",$account_id,$account_id);
+    $stmt->execute();
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+$app->post('/accounts/{id}/preleva', function (Request $request, Response $response) {
+
+    $conn = getConnection();
+    $data = $request->getParsedBody();
+    $account_id = $args['id'];
+
+    $stmt = $conn->prepare("INSERT INTO transactions (type, amount,description,account_id) VALUES (?, ?, ?,?)");
+    $stmt->bind_param("sisi", "withdrawal", $data['amount'], $data['description'],$account_id);
+    $stmt->execute();
+
+    $response->getBody()->write(json_encode([
+        "message" => "Prelevio eseguito",
+        "id" => $stmt->insert_id
+    ]));
+    $stmt = $conn->prepare("UPDATE accounts a SET currency = (select SUM(t.amount) from transactions t where t.account_id = ? ) WHERE a.id = ?");
+    $stmt->bind_param("ii",$account_id,$account_id);
+    $stmt->execute();
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+$app->get('/accounts/{id}/movimenti', function (Request $request, Response $response) {
+
+    $conn = getConnection();
+    $data = $request->getParsedBody();
+    $account_id = $args['id'];
+
+    $stmt = $conn->prepare("SELECT * from transactions t WHERE t.account_id = ?");
+    $stmt->bind_param("i",$account_id);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $movimenti = $result->fetch_assoc();
+
+    $response->getBody()->write(json_encode($movimenti));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+$app->get('/accounts/{id}/saldo', function (Request $request, Response $response) {
+
+    $conn = getConnection();
+    $data = $request->getParsedBody();
+    $account_id = $args['id'];
+
+    $stmt = $conn->prepare("SELECT currency from account a WHERE a.id = ?");
+    $stmt->bind_param("i",$account_id);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $saldo = $result->fetch_assoc();
+
+    $response->getBody()->write(json_encode($saldo));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+
+$app->get('/accounts/{id}/transazioni', "bankingControll:lista");
 
 
 // ottenere l'elenco dettagliato di un movimento
