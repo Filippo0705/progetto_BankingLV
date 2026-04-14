903c121c4b7984a11ffceaaaa750899980cfd333
student <student@itismeucci.com>
2026-04-14 13:18:50 +0200

    Final

--- build/init.sql
+++ build/init.sql
@@ -2,7 +2,7 @@
 CREATE TABLE accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     owner_name VARCHAR(255) NOT NULL,
-    currency BIGINT NOT NULL DEFAULT 'EUR',
+    currency BIGINT NOT NULL ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
